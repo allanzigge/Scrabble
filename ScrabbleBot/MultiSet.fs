6ba3fa5a4dea72a03a9df41f419831b7c7fd2d0b
Allan <asia@itu.dk>
2024-04-03 11:43:09 +0200

    add existing implementations

--- ScrabbleBot/MultiSet.fs
+++ ScrabbleBot/MultiSet.fs
@@ -2,8 +2,38 @@
 
 module internal MultiSet
 
-    type MultiSet<'a> = Temp of unit // Not implemented
+    type MultiSet<'a when 'a : comparison> = R of Map<'a, uint32> 
 
-    let empty : MultiSet<'a> = Temp () // Not implemented
-    let add   : 'a -> uint32 -> MultiSet<'a> -> MultiSet<'a> = fun _ _ _ -> failwith "Not implemented" 
-    let fold  : ('b -> 'a -> uint32 -> 'b) -> 'b -> MultiSet<'a> -> 'b = fun _ _ _ -> failwith "Not implemented"
+    let empty = R Map.empty
+
+    let isEmpty (R m) = Map.isEmpty m
+    
+    let size (R m) : uint32 = Map.fold (fun acc _ count -> count + acc) 0u m
+
+    let contains (search : 'a) (R m : MultiSet<'a>) : bool =
+        Map.containsKey search m 
+
+    let numItems (item : 'a) (R m : MultiSet<'a>) : uint32 = 
+       m.TryFind(item) |> Option.defaultValue 0u
+
+    let add (a:'a)(n: uint32)(R m: MultiSet<'a>) : MultiSet<'a> = 
+        let num : uint32 = numItems a (R m)
+        R(Map.add  a (num+n) m)
+
+    let addSingle (item : 'a) (s : MultiSet<'a>) : MultiSet<'a> = 
+        add (item) (1u) (s)
+
+    let remove (a : 'a) (n : uint32) (R(s)) : MultiSet<'a> = 
+        if (n >= (numItems a (R(s))))
+        then R (Map.remove a s)
+        else R (Map.add a ((numItems a (R(s)))-n) s)
+
+    let removeSingle (item : 'a) (R m : MultiSet<'a>) : MultiSet<'a> = 
+        remove (item) (1u) (R m)        
+    
+    let fold (f : 'b -> 'a -> uint32 -> 'b) (x : 'b) (R(s)) = 
+        Map.fold (f) x s
+
+    let foldBack (f : 'a -> uint32 -> 'b -> 'b) (R(s)) (x : 'b) = 
+        Map.foldBack (f) s x
+    
